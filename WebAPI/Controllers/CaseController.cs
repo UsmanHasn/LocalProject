34018978fc423bda0890994e6917f88cb7d00652
ughani <ughani@SJCDEVCL07>
2023-09-06 06:23:44 -0700

    Reverted

--- WebAPI/Controllers/CaseController.cs
+++ WebAPI/Controllers/CaseController.cs
@@ -172,7 +172,7 @@ namespace WebAPI.Controllers
         public IActionResult GetCasesByUserName(string UserName)
         {
             CaseModel model = new CaseModel();
-            //model = _caseService.GetCasesByUserName(UserName);
+            model = _caseService.GetCasesByUserName(UserName);
             return new JsonResult(new { data = model, status = HttpStatusCode.OK });
         }
 
